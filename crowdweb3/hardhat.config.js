7d063f5810da9085d1cd3394140d0eac20f1845c
Ridan Hawy <ridanhawy@gmail.com>
2025-06-23 19:09:25 +0300

    Initial Contracts

--- crowdweb3/hardhat.config.js
+++ crowdweb3/hardhat.config.js
@@ -1,12 +1,11 @@
-require("@matterlabs/hardhat-zksync-solc");
-require("@matterlabs/hardhat-zksync-verify");
-
+require('@matterlabs/hardhat-zksync-solc');
+require('@matterlabs/hardhat-zksync-verify');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   zksolc: {
-    version: "1.4.1",
-    compilerSource: "binary",
+    version: '1.4.1',
+    compilerSource: 'binary',
     settings: {
       optimizer: {
         enabled: true,
@@ -15,30 +14,30 @@ module.exports = {
   },
   networks: {
     zkSyncSepoliaTestnet: {
-      url: "https://sepolia.era.zksync.dev",
-      ethNetwork: "sepolia",
+      url: 'https://sepolia.era.zksync.dev',
+      ethNetwork: 'sepolia',
       zksync: true,
       chainId: 300,
       verifyURL:
-        "https://explorer.sepolia.era.zksync.dev/contract_verification",
+        'https://explorer.sepolia.era.zksync.dev/contract_verification',
     },
     zkSyncMainnet: {
-      url: "https://mainnet.era.zksync.io",
-      ethNetwork: "mainnet",
+      url: 'https://mainnet.era.zksync.io',
+      ethNetwork: 'mainnet',
       zksync: true,
       chainId: 324,
       verifyURL:
-        "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
+        'https://zksync2-mainnet-explorer.zksync.io/contract_verification',
     },
   },
   paths: {
-    artifacts: "./artifacts-zk",
-    cache: "./cache-zk",
-    sources: "./contracts",
-    tests: "./test",
+    artifacts: './artifacts-zk',
+    cache: './cache-zk',
+    sources: './contracts',
+    tests: './test',
   },
   solidity: {
-    version: "0.8.23",
+    version: '0.8.23',
     settings: {
       optimizer: {
         enabled: true,
