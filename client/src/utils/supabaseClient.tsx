17a569052e6de3d82a0b796721721104c6d71aeb
Ridan Hawy <ridanhawy@gmail.com>
2025-07-22 16:16:43 +0300

    connectin

--- client/src/utils/supabaseClient.tsx
+++ client/src/utils/supabaseClient.tsx
@@ -1,7 +1,7 @@
 // src/utils/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!; // Use service role key for backend
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
